d422e8141657def529a42cc437ae0bfbcf57945e
Ekaterna <edbelova@outlook.com>
2026-01-20 20:16:42 -0800

    feat: enhance ItemForm to dynamically display parent and child category names based on active category

--- src/features/items/components/ItemForm/ItemForm.tsx
+++ src/features/items/components/ItemForm/ItemForm.tsx
@@ -3,13 +3,28 @@ import { ItemImagesForm } from '../ItemImagesForm/ItemImagesForm'
 import { ItemDetailsForm } from '../ItemDetailsForm/ItemDetailsForm'
 import { ItemDescriptionForm } from '../ItemDescriptionForm/ItemDescriptionForm'
 import { ItemFormActions } from '../ItemFormActions/ItemFormActions'
+import { useCategories } from '../../../categories/queries/useCategories'
+import { useCategoryUiStore } from '../../../categories/state/useCategoryUiStore'
 
 
 export function ItemForm() {
+  const { data: categories = [], isLoading, isError } = useCategories()
+  const activeCategoryId = useCategoryUiStore((state) => state.activeCategoryId)
+
+  if (isLoading || isError) {
+    return null;
+  }
+
+  const parentCategory = categories.find((category) =>
+      category.children?.some((child) => child.id === activeCategoryId)
+    )
+
+  const childCategory = parentCategory?.children?.find((child) => child.id === activeCategoryId)
+
   return (
     <div className={styles.itemForm}>
       <div className={styles.directory}>
-        <h1 className={styles.title}>Figures /</h1>
+        <h1 className={styles.title}>{parentCategory?.name} / {childCategory?.name}</h1>
       </div>
       <div className={styles.itemLayout}>
         <div className={styles.itemImage}>
--- src/pages/ItemCreatePage/ItemCreatePage.tsx
+++ src/pages/ItemCreatePage/ItemCreatePage.tsx
@@ -1,13 +1,8 @@
-import styles from './ItemCreatePage.module.css'
 import { ItemForm } from '../../features/items/components/ItemForm/ItemForm'
-import { useCategories } from '../../features/categories/queries/useCategories'
-import { useCategoryUiStore } from '../../features/categories/state/useCategoryUiStore'
 
+import styles from './ItemCreatePage.module.css'
 
 export function ItemCreatePage() {
-  const { data: categories = [], isLoading, isError } = useCategories()
-  const activeCategoryId = useCategoryUiStore((state) => state.activeCategoryId)
-  
   return (
     <div className={styles.createItemPage}>
       <ItemForm />
