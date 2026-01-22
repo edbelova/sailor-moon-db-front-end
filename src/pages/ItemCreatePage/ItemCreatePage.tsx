f95c435add794e64e590fe895d626a6fa703cf5c
Ekaterna <edbelova@outlook.com>
2026-01-21 16:47:46 -0800

    feat: enhance ItemCreatePage with active category validation and user prompt

--- src/pages/ItemCreatePage/ItemCreatePage.tsx
+++ src/pages/ItemCreatePage/ItemCreatePage.tsx
@@ -1,8 +1,22 @@
+import { useCategoryUiStore } from '../../features/categories/state/useCategoryUiStore'
 import { ItemForm } from '../../features/items/components/ItemForm/ItemForm'
 
 import styles from './ItemCreatePage.module.css'
 
 export function ItemCreatePage() {
+  const activeCategory = useCategoryUiStore((state) => state.activeCategory);
+  console.log('Active Category:', activeCategory);
+
+  if (!activeCategory || !activeCategory.parent) {
+    return (
+      <div className={styles.createItemPage}>
+        <p className={styles.message}>
+          Please select a category to add an item.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.createItemPage}>
       <ItemForm />
